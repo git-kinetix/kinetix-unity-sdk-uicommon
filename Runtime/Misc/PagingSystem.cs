a600b91388903961e768309369399bce86a823c4
Thanatix <thomaslambert@kinetix.tech>
2023-12-04 16:01:46 +0100

    Version 1.2.0,

---
+++ Runtime/Misc/PagingSystem.cs
@@ -0,0 +1,62 @@
+// // ----------------------------------------------------------------------------
+// // <copyright file="InventoryFavoritePaging.cs" company="Kinetix SAS">
+// // Kinetix Unity SDK - Copyright (C) 2022 Kinetix SAS
+// // </copyright>
+// // ----------------------------------------------------------------------------
+
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using Kinetix.UI.Common.Translation;
+using UnityEngine.EventSystems;
+
+namespace Kinetix.UI.Common
+{
+    public class PagingSystem : MonoBehaviour
+    {
+        [SerializeField] private string          suffix;
+        [SerializeField] private Button          buttonLeft;
+        [SerializeField] private Button          buttonRight;
+        [SerializeField] private KinetixUITranslator labelPage;
+
+        public UnityAction OnSwitchPreviousPage;
+        public UnityAction OnSwitchNextPage;
+
+        public void Init()
+        {
+            buttonLeft.onClick.AddListener(OnSwitchPreviousPage);
+            buttonRight.onClick.AddListener(OnSwitchNextPage);
+        }
+
+        public void UpdatePageLabel(int _Page, int _TotalPage)
+        {
+            if(_TotalPage>1)
+            {
+                gameObject.SetActive(true);
+                labelPage.text = suffix + (_Page + 1) + "/" + _TotalPage;
+            }
+            else 
+            {
+                if (this != null)
+                    gameObject.SetActive(false);
+            }
+        }
+
+        public void OnDestroy()
+        {
+            buttonLeft.onClick.RemoveAllListeners();
+            buttonRight.onClick.RemoveAllListeners();
+        }
+
+        public void ClickLeft()
+        {
+            ExecuteEvents.Execute(buttonLeft.gameObject, new BaseEventData(EventSystem.current), ExecuteEvents.submitHandler);
+        }
+
+        public void ClickRight()
+        {
+            ExecuteEvents.Execute(buttonRight.gameObject, new BaseEventData(EventSystem.current), ExecuteEvents.submitHandler);
+        }
+    }
+}
